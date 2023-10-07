561d872bdb23428ed864604fa77762a1fc29529b
Sergi Guzman <sergi.guzman@glovoapp.com>
2023-10-08 01:53:50 +0200

    [NO-TICKET] feat: implementation

--- src/script/controller/input.ts
+++ src/script/controller/input.ts
@@ -24,35 +24,40 @@ export class Input {
   }
 
   static habdleKeyDown(e: KeyboardEvent) {
-    e.preventDefault()
     switch (e.key) {
       case 'ArrowLeft': {
+        e.preventDefault()
         this.left.isPressed = true
         this.left.onCurrentFrame = true
         break
       }
       case 'ArrowRight': {
+        e.preventDefault()
         this.right.isPressed = true
         this.right.onCurrentFrame = true
         break
       }
       case 'ArrowDown': {
+        e.preventDefault()
         this.down.isPressed = true
         this.down.onCurrentFrame = true
         break
       }
       case 'Shift': {
+        e.preventDefault()
         this.rotate.isPressed = true
         this.rotate.onCurrentFrame = true
         break
       }
       case 'Enter': {
+        e.preventDefault()
         this.enter.isPressed = true
         this.enter.onCurrentFrame = true
         break
       }
       case 'P':
       case 'p': {
+        e.preventDefault()
         this.pause.isPressed = true
         this.pause.onCurrentFrame = true
         break
@@ -64,32 +69,38 @@ export class Input {
     e.preventDefault()
     switch (e.key) {
       case 'ArrowLeft': {
+        e.preventDefault()
         this.left.isPressed = false
         this.left.onCurrentFrame = true
         break
       }
       case 'ArrowRight': {
+        e.preventDefault()
         this.right.isPressed = false
         this.right.onCurrentFrame = true
         break
       }
       case 'ArrowDown': {
+        e.preventDefault()
         this.down.isPressed = false
         this.down.onCurrentFrame = true
         break
       }
       case 'Shift': {
+        e.preventDefault()
         this.rotate.isPressed = false
         this.rotate.onCurrentFrame = true
         break
       }
       case 'Enter': {
+        e.preventDefault()
         this.enter.isPressed = false
         this.enter.onCurrentFrame = true
         break
       }
       case 'P':
       case 'p': {
+        e.preventDefault()
         this.pause.isPressed = false
         this.pause.onCurrentFrame = true
         break
