ae65105692fe47095263887d467c8040cf5c2166
Sergi Guzman <sergi.guzman@glovoapp.com>
2023-10-12 02:01:46 +0200

    [NO-TICKET] feat: repo setup

--- src/script/controller/input.ts
+++ src/script/controller/input.ts
@@ -35,7 +35,7 @@ export class Input {
   }
 
   static handleTapStart(key: string) {
-    const event = new KeyboardEvent('keydown', { key })
+    const event = new KeyboardEvent('keydown', { key });
     this.handleKeyDown(event)
   }
 
